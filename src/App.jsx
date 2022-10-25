989e4e2c690147271b06d4f6a27452f4fffc5b73
Chema Torres <chemaaa94@gmail.com>
2022-10-25 16:12:41 -0500

    creacion del portafolio usando REACT

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experince from "./components/experience/Experience";
+import Service from "./components/service/Service";
+import Portafolio from "./components/service/Service";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experince />
+      <Service />
+      <Portafolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
