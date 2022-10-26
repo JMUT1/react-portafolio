6096e7f1356a90a78002cc25947f138d7aceb0e2
Chema Torres <chemaaa94@gmail.com>
2022-10-25 21:39:26 -0500

    Header progress

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I´m</h5>
+        <h1>José María Ureña Torres</h1>
+        <h5 className="text-light">Jr. Front-End Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="me" />
+          <a href="#contact" className="scroll__down">
+            Scroll down
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="__blank">
+        Linkedin
+      </a>
+      <a href="https://github.com" target="__blank">
+        Github
+      </a>
+      <a href="https://dribble.com" target="__blank">
+        Dribble
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
