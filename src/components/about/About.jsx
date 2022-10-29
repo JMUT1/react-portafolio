3bc79ebb42b33e3f45f4c7321fd907f76083791f
Chema Torres <chemaaa94@gmail.com>
2022-10-29 12:14:26 -0500

    JSX about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { BsFolderCheck } from "react-icons/bs";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about__me-image">
+          <img src={ME} alt="" />
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            {/* CARD 1 */}
+            <article className="about__card">
+              <FaAward />
+              <h5>Experience</h5>
+              <small>2+ Year Working </small>
+            </article>
+            {/* CARD 2 */}
+            <article className="about__card">
+              <FiUsers />
+              <h5>Clients</h5>
+              <small>200 + Worldwide </small>
+            </article>
+            {/* CARD 3 */}
+            <article className="about__card">
+              <BsFolderCheck />
+              <h5>Projects</h5>
+              <small>80+ complet projects </small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+            hic asperiores fugit quos expedita sed debitis nihil voluptas,
+            voluptate, inventore fuga cum omnis accusamus ullam modi ab! Sit,
+            labore perferendis!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            {" "}
+            Let´s Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
