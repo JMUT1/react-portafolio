6215da7cacc76de6322e66d5c78ad077d7a5aadd
Chema Torres <chemaaa94@gmail.com>
2022-10-29 19:06:46 -0500

    JSX structure EXPERICENCE COMPONENTS

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        {/* FRONTEND EXPERIENCE */}
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          {/* CONTENEDOR DONDE SE VA TRABAJAR CON LOS ARTICULOS INDIVIDUALES */}
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-ligth">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-ligth">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className="text-ligth">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-ligth">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className="text-ligth">Experienced</small>
+            </article>
+          </div>
+        </div>
+        {/* BACKEND CONTENEDORES */}
+        <div className="experience__backend"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
